fcbf8614ef6fdf2b8da259a98583b75237dd2cb4
MrHamedi <ahmadihamed167@gmail.com>
2021-05-04 01:27:43 +0430

    question detail page added

--- src/question/admin.py
+++ src/question/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question,Review
 
 
 class QuestionAdmin(admin.ModelAdmin):
+    """
+        This is the admin class for Question model 
+    """
+
     readonly_fields=("publish","update")
     list_display=("author","score","publish")
     list_filter=("author","score")
@@ -12,5 +16,24 @@ class QuestionAdmin(admin.ModelAdmin):
     )
 
 
+class ReviewAdmin(admin.ModelAdmin):
+    """
+            This is the admin class for Review model 
+    """
+    readonly_fields=("update","publish")
+    list_display=("question","author","publish","update")
+    list_filter=("question","author","publish")
+    search_fields=("content","pk")
+    fieldsets=(
+        (None,{"fields":("question","publish","update")}),
+        ("Info",{"fields":("score","content"),"classes":"collapse"})
+    )
+
+    class Meta:
+        ordering=("publish",)
+
+
+
 # Register your models here.
 admin.site.register(Question,QuestionAdmin)
+admin.site.register(Review,ReviewAdmin)
--- src/question/models.py
+++ src/question/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib import auth 
 from taggit.managers import TaggableManager
 from django.urls import reverse
+from django.contrib.auth.models import User 
 
 
 class Question(models.Model):
@@ -13,7 +14,7 @@ class Question(models.Model):
     publish=models.DateTimeField(auto_now_add=True,help_text="The publish date of post")
     update=models.DateTimeField(verbose_name="The last update",auto_now=True)
     author=models.ForeignKey(auth.get_user_model(),on_delete=models.CASCADE,related_name="questions")
-    score=models.DecimalField(max_digits=3,default=0,decimal_places=1)
+    score=models.IntegerField(help_text="The score of this question")
     slug=models.SlugField(max_length=200,unique=True)
     tags=TaggableManager()
 
@@ -27,4 +28,22 @@ class Question(models.Model):
     def __str__(self):
         return(self.title)
 
-    
+
+class Review(models.Model):
+        #This model is for client reviews on question
+    author=models.ForeignKey(auth.get_user_model(),on_delete=models.SET_DEFAULT ,default=1,help_text="The author of review")
+    question=models.ForeignKey(Question,on_delete=models.PROTECT,help_text="The question that this review has been written for",null=True,related_name="reviews")
+    content=models.TextField()
+    score=models.IntegerField(help_text="The score of this review")    
+    publish=models.DateTimeField(help_text="The publish time of this review",verbose_name="Publish time",auto_now_add=True)
+    update=models.DateTimeField(help_text="The time of the last editting of this review",verbose_name="Last update time",auto_now=True)
+
+    class Meta:
+        ordering=("publish",)
+
+    def __str__(self):
+        return(self.content)
+
+
+
+
