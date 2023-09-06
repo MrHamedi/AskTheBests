b59cf4cefa267520693268d54a97f323e8eb89db
MrHamedi <ahmadihamed167@gmail.com>
2023-09-06 14:54:12 +0330

    custome user will be used as user

--- src/AB/settings.py
+++ src/AB/settings.py
@@ -92,8 +92,12 @@ WSGI_APPLICATION = 'AB.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("DB_NAME"),
+        'HOST': 'db',
+        'USER':os.environ.get("DB_USER"),
+        'PASSWORD':os.environ.get("DB_PASSWORD"),
+        'PORT':5432,
     }
 }
 
