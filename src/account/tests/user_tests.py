8d3be4e156fa1673100752161a40e7b3bdfc7c34
MrHamedi <ahmadihamed167@gmail.com>
2023-09-22 18:18:08 +0330

    new tests for token creation added

--- src/account/tests/user_tests.py
+++ src/account/tests/user_tests.py
@@ -7,7 +7,7 @@ from rest_framework import status
 
 
 def create_user(**params):
-    return get_user_model().objects.createuser(**params)
+    return get_user_model().objects.create_user(**params)
 
 
 class PublicUserCreationTestCase(TestCase):
@@ -50,3 +50,49 @@ class PublicUserCreationTestCase(TestCase):
                                     password="testpassword"
                                     )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+
+class PublicUserLoginTest(TestCase):    
+
+    TOKEN_URL=reverse("account:token")
+    valid_credentials_payload={
+        'email' : 'test@email.com',
+        'password' : 'testpassword123'
+    }
+    non_existing_user_payload={
+        'email' : "doesnotexist@mail.com",
+        'password' : 'testpassword123',
+    }
+    invalid_credentials_payload={
+        'email' : 'test@email.com',
+        'password' : 'pass'
+    }
+    no_password_paylad={
+        'email' : 'test@email.com'
+    }
+
+
+    def setUp(self):
+        self.user=create_user(**self.valid_credentials_payload)
+        self.client = APIClient()
+
+    def test_token_creation(self):
+        response=self.client.post(self.TOKEN_URL, self.valid_credentials_payload)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('token', response.data)
+
+    def test_token_creation_with_non_existing_user(self):
+        response=self.client.post(self.TOKEN_URL, self.non_existing_user_payload)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertNotIn('token', response.data)
+
+    def test_token_creation_invalid_credentials(self):
+        response=self.client.post(self.TOKEN_URL, self.invalid_credentials_payload)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertNotIn('token', response.data)
+
+    def test_token_creation_no_password(self):
+        response=self.client.post(self.TOKEN_URL, self.no_password_paylad)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertNotIn('token', response.data)        
