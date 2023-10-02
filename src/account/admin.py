7bb56b655d1cd14a6802e14f24a1f11278a56f21
MrHamedi <ahmadihamed167@gmail.com>
2023-10-02 22:48:04 +0330

    first_name last_name and username removed from admin

--- src/account/admin.py
+++ src/account/admin.py
@@ -6,15 +6,9 @@ from django.contrib.auth.models import User
 
 
 class ProfileAdmin(admin.ModelAdmin):
-    list_display=("user","name","pic","code")
-    search_fields=("user__username","code","user__first_name","user__last_name")
+    list_display=("user","pic","code")
+    search_fields=("user__email","code")
     date_hierarchy="user__date_joined"
-
-    """
-        This function will return the full name of user 
-    """
-    def name(self,obj):
-        return(obj.user.first_name+" "+obj.user.last_name)
 
 
 admin.site.register(Profile,ProfileAdmin)
