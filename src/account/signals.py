87464af803db191a321fff73295250e26317f024
MrHamedi <ahmadihamed167@gmail.com>
2023-10-08 21:26:50 +0330

    create_profile signal added

---
+++ src/account/signals.py
@@ -0,0 +1,12 @@
+from django.db.models.signals import post_save
+from django.contrib.auth import get_user_model 
+from django.dispatch import receiver
+
+from .models import Profile 
+
+
+@receiver(post_save, sender=get_user_model())
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        profile = Profile(user=instance)
+        profile.save()
