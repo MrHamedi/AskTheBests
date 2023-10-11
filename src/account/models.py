b1ae5e7b9b59f1b846141ee16c2d5547827a868d
MrHamedi <ahmadihamed167@gmail.com>
2023-10-11 23:08:51 +0330

    Profile model moved to core

--- src/account/models.py
+++ src/account/models.py
@@ -1,14 +1,4 @@
 from django.db import models
-from django.contrib.auth import get_user_model
 
 
-class Profile(models.Model):
-    pic = models.ImageField(upload_to="profile_image/%Y/%m/%d/",
-                            verbose_name="picture", null=True, blank=True)
-    code = models.CharField(max_length=6, null=True)
-    code_limit_time = models.DateTimeField(
-        help_text="The expiration date of this activsion code", null=True, blank=True)
-    score = models.IntegerField(
-        help_text="The score of this person", default=0)
-    user = models.OneToOneField(
-        get_user_model(), on_delete=models.CASCADE, related_name="profile")
+
